b91424d237cadf79d04007feae1f57fe4d6e8a3e
Sebastian McKee <sebmcke@gmail.com>
2024-11-28 23:03:26 -0600

    Create trick_or_treater class

--- ruby_exercises/objects-and-methods/exercise-1/lib/trick_or_treater.rb
+++ ruby_exercises/objects-and-methods/exercise-1/lib/trick_or_treater.rb
@@ -1,3 +1,27 @@
+require_relative "bag"
+
 class TrickOrTreater
-    
+    attr_reader :costume
+    attr_accessor :bag
+
+    def initialize(costume)
+        @costume = costume
+        @bag = Bag.new
+    end
+
+    def dressed_up_as
+        costume.style
+    end
+
+    def has_candy? 
+        !bag.empty?
+    end
+
+    def candy_count
+        bag.count
+    end
+
+    def eat
+        bag.candies.pop
+    end
 end
